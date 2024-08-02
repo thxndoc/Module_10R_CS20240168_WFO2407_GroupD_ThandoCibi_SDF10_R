24b8173e19ae6b711ffb7541f60d445d2005c722
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-08-02 11:04:48 +0200

    Create functions for button

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ addButtonEl.addEventListener("click", function() {
     
     push(shoppingListInDB, inputValue)
 
-    inputFieldEl.value = ""
+    clearInputFieldEl()
 
-    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+    appendItemToShoppingListEl(inputValue)
 })
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
