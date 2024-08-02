d5d97d55696dd94693b1e5d85bf38d98bb0ae2f9
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-08-02 09:27:00 +0200

    Edit comment

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
 /*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
+When 'Add to cart' button is clicked, whatever is written in the input field should be console logged.
 */
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
