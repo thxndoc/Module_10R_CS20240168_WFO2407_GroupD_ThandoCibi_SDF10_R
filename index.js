4ed57c7418540ad81e06d8cf202f23dc8c212089
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-08-02 10:29:42 +0200

    Add Firebase to project

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
-    databaseURL: "https://playground-18915-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://realtime-database-9a1a8-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 //When 'Add to cart' button is clicked, console log whatever is typed in input field
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -8,5 +15,7 @@ const addButtonEl = document.getElementById("add-button")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
 })
