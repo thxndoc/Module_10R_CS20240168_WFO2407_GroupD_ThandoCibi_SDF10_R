887cdcdf6e2859dc98074a76b1e3c9ffd86951ae
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-08-02 09:58:35 +0200

    Add database URL

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-/*
-When 'Add to cart' button is clicked, whatever is written in the input field should be console logged.
-*/
+const appSettings = {
+    databaseURL: "https://playground-18915-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+//When 'Add to cart' button is clicked, console log whatever is typed in input field
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
